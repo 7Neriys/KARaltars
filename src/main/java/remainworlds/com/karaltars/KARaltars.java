c41e45cc2fd5abb4925aa42e12315cd9a261a41a
7Neriys <7Neriys728@gmail.com>
2022-12-29 23:24:08 +0300

    Init commit

---
+++ src/main/java/remainworlds/com/karaltars/KARaltars.java
@@ -0,0 +1,17 @@
+package remainworlds.com.karaltars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KARaltars extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
